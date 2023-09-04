0ac7bd0892ec41c4b10c4c0a7bea34d6ea06d60c
Bas Visscher <bas.visscher24@gmail.com>
2023-09-04 18:01:34 +0200

    Download gta map

--- Mappr/Form1.cs
+++ Mappr/Form1.cs
@@ -1,4 +1,5 @@
 using Mappr.Controls;
+using System.Net;
 
 namespace Mappr
 {
@@ -6,6 +7,7 @@ namespace Mappr
     {
         MapView mapView = new MapView();
         FileTileSource mapTileSource;
+        FileDownloader downloader = new FileDownloader();
         public Form1()
         {
             InitializeComponent();
@@ -13,17 +15,97 @@ namespace Mappr
             mapView.Location = new System.Drawing.Point(20, 20);
             mapView.Size = new System.Drawing.Size(1024, 1024);
             mapView.BorderStyle = BorderStyle.FixedSingle;
-            mapTileSource = new FileTileSource("maps");
+            mapTileSource = new FileTileSource("maps/gta5");
             mapView.TileSource = mapTileSource;
+            //Download();
+        }
+
+
+        string GetUri(int z, int x, int y)
+        {
+            return $"https://s.rsg.sc/sc/images/games/GTAV/map/render/{z}/{x}/{y}.jpg";
+        }
+
+        string GetFilePath(int z, int x, int y)
+        {
+            return $"maps/gta5/{z}/{x}x{y}.jpg";
         }
 
         private void mapView1_Load(object sender, EventArgs e)
         {
+        
         }
+
+        async Task Download()
+        {
+            for (int z = 0; z < 8; z++)
+            {
+                int max = 1 << z;
+                for (int y = 0; y < max; y++)
+                {
+                    for (int x = 0; x < max; x++)
+                    {
+                        var uri = GetUri(z, x, y);
+                        var file = GetFilePath(z, x, y);
+                        await downloader.DownloadFileAsync(uri, file);
+                    }
+                }
+            }
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             mapView.Redraw();
+        }
+    }
+
+
+    public class FileDownloader
+    {
+        private readonly HttpClient httpClient;
+
+        public FileDownloader()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task DownloadFileAsync(string url, string localFilePath)
+        {
+            try
+            {
+                // Extract the directory path from the local file path.
+                string directoryPath = Path.GetDirectoryName(localFilePath);
+
+                // Check if the directory exists; if not, create it.
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    byte[] fileBytes = await response.Content.ReadAsByteArrayAsync();
+                    File.WriteAllBytes(localFilePath, fileBytes);
+                    Console.WriteLine($"File downloaded and saved to {localFilePath}");
+                }
+                else
+                {
+                    Console.WriteLine($"HTTP request failed with status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            httpClient.Dispose();
         }
     }
 }
