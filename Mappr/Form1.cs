fa22fe68115c105ac7bed6684fbe1bd922038082
Bas Visscher <bas.visscher24@gmail.com>
2024-04-28 22:10:57 +0200

    Getting coordinates begin

--- Mappr/Form1.cs
+++ Mappr/Form1.cs
@@ -6,11 +6,17 @@ using Mappr.Kernel;
 using Mappr.MapInteractions;
 using Mappr.Tiles;
 using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
 using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks.Dataflow;
 
 namespace Mappr
 {
+
     public partial class Form1 : Form
     {
         MapView mapView = new MapView();
@@ -54,7 +60,7 @@ namespace Mappr
 
 
 
-            FileTileSource fileSource = new FileTileSource("maps/tarkov/interchange");
+            FileTileSource fileSource = new FileTileSource("maps/tarkov/woods");
             ScalerTileSource scaler = new ScalerTileSource(fileSource);
             CachingTileSource cashing = new CachingTileSource(scaler, (1920 * 1080) * 5 / (128 * 128));
             mapView.TileSource = cashing;
@@ -85,136 +91,236 @@ namespace Mappr
 
 
 
-            worldPoints.AddRange(new Vector2[] {
-                new Vector2(3259.0862f, 5148.1704f),
-                 new Vector2(-1660.7303f, -1029.1011f),
-            });
-            
-            mapPoints.AddRange(new Vector2[] {
-                new Vector2(105.21094f, 46.25f),
-                 new Vector2(35.265625f, 134.11719f),
-            });
+            //worldPoints.AddRange(new Vector2[] {
+            //    new Vector2(3259.0862f, 5148.1704f),
+            //     new Vector2(-1660.7303f, -1029.1011f),
+            //});
+            //
+            //mapPoints.AddRange(new Vector2[] {
+            //    new Vector2(105.21094f, 46.25f),
+            //     new Vector2(35.265625f, 134.11719f),
+            //});
 
 
-            scaler2 = CoordinateRegression.Fit(worldPoints.ToArray(), mapPoints.ToArray());
+            //scaler2 = CoordinateRegression.Fit(worldPoints.ToArray(), mapPoints.ToArray());
 
             Start();
+
+
+
+
+        }
+
+        void PositionUpdated()
+        {
+            this.InvokeIfRequired(() => {
+                this.Text = playerWorldPos.ToString("F3");
+            });
+
+
+            if (scaler2 == null)
+                return;
+
+            var playerMapPos = scaler2.ApplyTransformation(playerWorldPos);
+            playerEntity.MapPosition = playerMapPos;
+            mapView.InvokeIfRequired(() =>
+            {
+                mapView.SetCenter(playerMapPos);
+                mapView.Redraw();
+            });
         }
 
 
         public void Start()
         {
-            Task.Run(() =>
+            Task.Run(async () =>
             {
                 while (true)
                 {
-                    if (!memoryManager.IsAttached())
+                    Vector3? worldPos = GetWorldPos();
+
+                    if(worldPos == null)
                     {
-                        if (!memoryManager.attach("EscapeFromTarkov"))
-                            return;
+                        await Task.Delay(1000);
                     }
 
-                    nint unityBase                  = memoryManager.GetProcessModuleBase("UnityPlayer.dll");
-                    nint gameObjectManager          = memoryManager.Read_Address( unityBase + 0x17FFD28);
+                    playerWorldPos = new Vector2(worldPos.Value.X, worldPos.Value.Y);
+                    PositionUpdated();
 
-
-                    nint LastTaggedNode             = memoryManager.Read_Address( gameObjectManager + 0x0  );
-                    nint TaggedNodes                = memoryManager.Read_Address( gameObjectManager + 0x8  );
-                    nint LastMainCameraTaggedNode   = memoryManager.Read_Address( gameObjectManager + 0x10 );
-                    nint MainCameraTaggedNodes      = memoryManager.Read_Address( gameObjectManager + 0x18 );
-                    nint LastActiveNode             = memoryManager.Read_Address( gameObjectManager + 0x20 );
-                    nint ActiveNodes                = memoryManager.Read_Address( gameObjectManager + 0x28 );
-
-                    nint activeNodes                = memoryManager.Read_Address(ActiveNodes);
-                    nint lastActiveNode             = memoryManager.Read_Address(LastActiveNode);
-
-                    nint cObject = activeNodes;
-                    nint gameWorld = 0;
-                    //TODO: doesnt include last item!
-                    while (cObject != lastActiveNode)
-                    {
-                        nint obj = memoryManager.Read_Address(cObject + 0x10);
-                        nint nameptr = memoryManager.Read_Address(obj + 0x60);
-                        string name = memoryManager.Read_String(nameptr, 128);
-
-                        if(name == "GameWorld")
-                        {
-                            gameWorld = obj;
-                            break;
-                        }
-                        cObject = memoryManager.Read_Address(cObject + 0x8);
-                    }
-
-                    if (gameWorld == 0)
-                        return;
-
-                    nint c1 = memoryManager.Read_Address(gameWorld + 0x30);
-                    nint c2 = memoryManager.Read_Address(c1 + 0x18);
-                    nint c3 = memoryManager.Read_Address(c2 + 0x28);
-                    nint mainPlayer = memoryManager.Read_Address(c3 + 0x118);
-
-
-                    nint weaponPort = memoryManager.Read_Address(mainPlayer + 0x1A0);
-                    nint hands = memoryManager.Read_Address(weaponPort + 0x18);
-                    nint fireport = memoryManager.Read_Address(hands + 0x88);
-                    nint test1 = memoryManager.Read_Address(fireport + 0x10);
-
-
-                    nint body = memoryManager.Read_Address(mainPlayer + 0xA8);
-                    nint playerbones = memoryManager.Read_Address(body + 0x20);
-                    nint root = memoryManager.Read_Address(playerbones + 0x80);
-                    nint test2 = memoryManager.Read_Address(root + 0x10);
-
-
-
-
-
-                    nint skele = memoryManager.Read_Address(body + 0x28);
-                    nint _values = memoryManager.Read_Address(skele +0x28);
-                    nint pbase = memoryManager.Read_Address(_values + 0x10);
-                    nint cnt = memoryManager.Read_Address(_values + 0x18);
-
-                    int boneIndex = 0;
-                    nint start = memoryManager.Read_Address(pbase + 0x20 + boneIndex * 0x8);
-                    nint test3 = memoryManager.Read_Address(start + 0x10);
-
-
-
-
-
-                    //IntPtr gameWorld = FindObject(activeNodes, lastActiveNode, "GameWorld");
-                    //
-                    //
-                    //
-                    //
-                    //_localGameWorld = Memory.ReadPtrChain(gameWorld, { 0x30, 0x18, 0x28});
-
-
-                    return;
-
-
-
-
-                        IntPtr xAddr = memoryManager.GetProcessBase() + 0x1D9F800;
-                    IntPtr yAddr = memoryManager.GetProcessBase() + 0x1D9F804;
-
-                    playerWorldPos = new Vector2(
-                        memoryManager.Read_Float(xAddr),
-                        memoryManager.Read_Float(yAddr));
-
-                    if (scaler2 == null)
-                        return;
-                    var playerMapPos = scaler2.ApplyTransformation(playerWorldPos);
-                    playerEntity.MapPosition = playerMapPos;
-                    mapView.InvokeIfRequired(() =>
-                    {
-                        mapView.SetCenter(playerMapPos);
-                        mapView.Redraw();
-                    });
                 }
             });
         }
+
+
+        Vector3? GetWorldPos()
+        {
+            if (!memoryManager.IsAttached())
+            {
+                if (!memoryManager.attach("EscapeFromTarkov"))
+                    return null;
+            }
+
+            nint unityBase = memoryManager.GetProcessModuleBase("UnityPlayer.dll");
+            nint gameObjectManager = memoryManager.Read_Address(unityBase + 0x17FFD28);
+
+
+            nint LastTaggedNode = memoryManager.Read_Address(gameObjectManager + 0x0);
+            nint TaggedNodes = memoryManager.Read_Address(gameObjectManager + 0x8);
+            nint LastMainCameraTaggedNode = memoryManager.Read_Address(gameObjectManager + 0x10);
+            nint MainCameraTaggedNodes = memoryManager.Read_Address(gameObjectManager + 0x18);
+            nint LastActiveNode = memoryManager.Read_Address(gameObjectManager + 0x20);
+            nint ActiveNodes = memoryManager.Read_Address(gameObjectManager + 0x28);
+
+            nint activeNodes = memoryManager.Read_Address(ActiveNodes);
+            nint lastActiveNode = memoryManager.Read_Address(LastActiveNode);
+
+            nint cObject = activeNodes;
+            nint gameWorld = 0;
+            //TODO: doesnt include last item!
+            while (cObject != lastActiveNode)
+            {
+                nint obj = memoryManager.Read_Address(cObject + 0x10);
+                nint nameptr = memoryManager.Read_Address(obj + 0x60);
+                string name = memoryManager.Read_String(nameptr, 128);
+
+                if (name == "GameWorld")
+                {
+                    gameWorld = obj;
+                    break;
+                }
+                cObject = memoryManager.Read_Address(cObject + 0x8);
+            }
+
+            if (gameWorld == 0)
+                return null;
+
+
+            nint c1 = memoryManager.Read_Address(gameWorld + 0x30);
+            nint c2 = memoryManager.Read_Address(c1 + 0x18);
+            nint c3 = memoryManager.Read_Address(c2 + 0x28);
+            nint playerBase = memoryManager.Read_Address(c3 + 0x118);
+
+            var ptr_1 = memoryManager.Read_Address(playerBase + 0xA8);
+            var ptr_2 = memoryManager.Read_Address(ptr_1 + 0x28);
+            var ptr_3 = memoryManager.Read_Address(ptr_2 + 0x28);
+            var ptr_4 = memoryManager.Read_Address(ptr_3 + 0x10);
+            var head_index = 133;
+            var ptr_5 = memoryManager.Read_Address(ptr_4 + 0x20 + (0x8 * head_index));
+            var transObj = memoryManager.Read_Address(ptr_5 + 0x10);
+
+            var matrix = memoryManager.Read_Address(transObj + 0x38);
+            var index = memoryManager.Read_Int32(transObj + 0x40);
+
+            var matrix_list = memoryManager.Read_Address(matrix + 0x18);
+            var matrix_indices = memoryManager.Read_Address(matrix + 0x20);
+
+            Vector3 result = memoryManager.Read_Vector3(matrix_list + 0x28 * index);
+            int transformIndex = memoryManager.Read_Int32(matrix_indices + 4 * index);
+
+
+            while (transformIndex >= 0)
+            {
+                var tMatrix = memoryManager.Read_Transform3(matrix_list + 0x28 * transformIndex);
+
+                float rotX = tMatrix.Rotation.X;
+                float rotY = tMatrix.Rotation.Y;
+                float rotZ = tMatrix.Rotation.Z;
+                float rotW = tMatrix.Rotation.W;
+
+                float scaleX = result.X * tMatrix.Scale.X;
+                float scaleY = result.Y * tMatrix.Scale.Y;
+                float scaleZ = result.Z * tMatrix.Scale.Z;
+
+                var x = tMatrix.Position.X + scaleX +
+                    (scaleX * ((rotY * rotY * -2.0f) - (rotZ * rotZ * 2.0f))) +
+                    (scaleY * ((rotW * rotZ * -2.0f) - (rotY * rotX * -2.0f))) +
+                    (scaleZ * ((rotZ * rotX * 2.0f) - (rotW * rotY * -2.0f)));
+                var y = tMatrix.Position.Y + scaleY +
+                    (scaleX * ((rotX * rotY * 2.0f) - (rotW * rotZ * -2.0f))) +
+                    (scaleY * ((rotZ * rotZ * -2.0f) - (rotX * rotX * 2.0f))) +
+                    (scaleZ * ((rotW * rotX * -2.0f) - (rotZ * rotY * -2.0f)));
+                var z = tMatrix.Position.Z + scaleZ +
+                    (scaleX * ((rotW * rotY * -2.0f) - (rotX * rotZ * -2.0f))) +
+                    (scaleY * ((rotY * rotZ * 2.0f) - (rotW * rotX * -2.0f))) +
+                    (scaleZ * ((rotX * rotX * -2.0f) - (rotY * rotY * 2.0f)));
+
+                result = new Vector3((float)x, (float)y, (float)z);
+
+                transformIndex = memoryManager.Read_Int32(matrix_indices + 4 * transformIndex);
+            }
+
+            return result;
+
+        }
     }
 
+
+
+ 
+
+
+
+
+    public static class Ext
+    {
+        public static Vector3 Read_Vector3(this MemoryManager memoryManager, nint address)
+        {
+            return new Vector3(
+                memoryManager.Read_Float(address + 0),
+                memoryManager.Read_Float(address + 4),
+                memoryManager.Read_Float(address + 8));
+        }
+
+        public static Vector4 Read_Vector4(this MemoryManager memoryManager, nint address)
+        {
+            return new Vector4(
+                memoryManager.Read_Float(address + 0),
+                memoryManager.Read_Float(address + 4),
+                memoryManager.Read_Float(address + 8),
+                memoryManager.Read_Float(address + 12));
+        }
+
+        public static Quaternion Read_Quaternion(this MemoryManager memoryManager, nint address)
+        {
+            return new Quaternion(
+                memoryManager.Read_Float(address + 12),
+                memoryManager.Read_Float(address + 16),
+                memoryManager.Read_Float(address + 20),
+                memoryManager.Read_Float(address + 24));
+        }
+
+        public static Transform3 Read_Transform3(this MemoryManager memoryManager, nint address)
+        {
+            Transform3 transform = new Transform3();
+            transform.Position = memoryManager.Read_Vector3(address);           // 0 4 8
+            transform.Rotation = memoryManager.Read_Quaternion(address + 12);   // 12 16 20 24
+            transform.Scale = memoryManager.Read_Vector3(address + 28);         // 28 32 36
+            return transform;
+        }
+        
+        public static Transform4 Read_Transform4(this MemoryManager memoryManager, nint address)
+        {
+            Transform4 transform = new Transform4();
+            transform.Position = memoryManager.Read_Vector4(address);           // 0 4 8 12
+            transform.Rotation = memoryManager.Read_Quaternion(address + 16);   // 16 20 24 28
+            transform.Scale = memoryManager.Read_Vector4(address + 32);         // 32 36 40 44
+            return transform;
+        }
+    }
+
+
+    public class Transform3
+    { 
+        public Vector3 Position { get; set; }       //3x 32
+        public Quaternion Rotation { get; set; }    //4x 32
+        public Vector3 Scale { get; set; }          //3x 32    
+    }
+
+    public class Transform4
+    {
+        public Vector4 Position { get; set; }       //3x 32
+        public Quaternion Rotation { get; set; }    //4x 32
+        public Vector4 Scale { get; set; }          //3x 32    
+    }
 
 }
